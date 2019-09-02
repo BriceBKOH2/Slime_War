ff3c94698cc69102635438fddfa5340729ac6e22
BriceB <brice.beteille@gmail.com>
2019-09-02 23:54:35 +0200

    Initial commit

---
+++ Assets/Spawn_Object.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_Object : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Spawn Board - tiles
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    
+        //Spawn Tiles for each player.		
+	}
+}
